d37654b1bce9fd4d2ca669a9f3d86a3d5873d6e8
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-02-01 22:09:02 +0000

    add routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} /> 
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 
