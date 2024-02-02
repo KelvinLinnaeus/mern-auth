89e1f91bacb162a4ce1c11823424770d1f4e2e0a
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-02-02 00:17:38 +0000

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
+import { error } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
+
 
 app.listen(3000, () => {
   console.log("Server Listening on port 3000")
 })
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Database")
+    
+  })
+  .catch(error => console.log(error))
